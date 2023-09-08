8c68f5286fa3242bf5d45f0db7d34303d9aebc80
ehdgusdl9177 <k23129177@gmail.com>
2023-09-08 10:06:02 +0900

    유저 세션 생성

--- public/main.js
+++ public/main.js
@@ -15,3 +15,39 @@ const userTagline = document.querySelector("#user-tagline");
 const title = document.getElementById("#active-user");
 const messages = document.querySelector(".messages");
 const msgDiv = document.querySelector(".msg-form");
+
+// login form handler
+const loginForm = document.querySelector(".user-login");
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const username = document.getElementsById("username");
+  createSession(username.value.toLowerCase());
+  username.value = "";
+});
+
+const createSession = async (username) => {
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username }),
+  };
+  await fetch("/session", options)
+    .then((res) => res.json())
+    .then((data) => {
+      socketConnect(data.username, data.userId);
+      // localstorage에 세션을 Set
+      localStorage.setItem("session-username", data.username);
+      localStorage.setItem("sesstion-userId", data.userId);
+
+      loginContainer.classList.add("d-none");
+      chatBody.classList.remove("d-none");
+      userTitle.innerHTML = data.username;
+    })
+    .catch((err) => console.log(err));
+};
+
+const socketConnect = async (username, userId) => {
+  socket.auth = { username, userId };
+
+  await socket.connect();
+};
